30cdd8cb1de72f3680802cbd086d302ac926c293
Shamil <ns-80@yandex.ru>
2022-12-14 19:59:23 +0500

    test 6

--- test/Spec.hs
+++ test/Spec.hs
@@ -9,7 +9,9 @@ import News (setLimitAndOffsetWith, setMethodNews)
 
 testsFunctionReadNum = do
   it "turns the Text to Int" $ do
-    readNum "23" `shouldBe` (23 :: Int)    
+    readNum "23" `shouldBe` (23 :: Int)
+    readNum "-23" `shouldBe` ((-23) :: Int) 
+    readNum "23.5" `shouldBe` (0 :: Int)     
     readNum "abc" `shouldBe` (0 :: Int) 
     readNum "23abc" `shouldBe` (0 :: Int)    
     readNum "abc23" `shouldBe` (0 :: Int)
@@ -51,9 +53,70 @@ testsFunctionSetLimitAndOffsetWith = do
     setLimitAndOffsetWith 20 [("offset",Just "S")] `shouldBe` 
       Query " LIMIT 20 OFFSET 0"       
 
-testsFunctionSetMethodNews = do                          -- Int -> [(T.Text, Maybe T.Text)] -> Maybe (Query, Query)
+testsFunctionSetMethodNews = do                          
   it "list is empty" $                                
     setMethodNews 20 [] `shouldBe` Just ("title LIKE '%'"," LIMIT 20 OFFSET 0") 
+  it "test 1" $                                
+    setMethodNews 20 
+      [("created_since",Just "2022-03-22"), ("author",Just "Ann")
+      ,("sort_by",Just "author"), ("limit",Just "10")
+      ,("offset",Just "1")
+      ] 
+      `shouldBe` 
+         Just ("News.creation_date >= '2022-03-22' AND name_user = 'Ann'",
+               "ORDER BY author.name_user LIMIT 10 OFFSET 1")
+  it "test 2" $                                
+    setMethodNews 20 
+      [("created_since",Just "2022-03-22"), ("author",Just "Ann")
+      ,("sort_by",Just "foto"), ("limit",Just "10")
+      ,("offset",Just "1")
+      ] `shouldBe` Nothing
+  it "test 3" $                                
+    setMethodNews 20 
+      [("created_at",Just "2022-03-22"), ("author",Just "Sam")
+      ,("category",Just "11"),("sort_by",Just "photo"), ("limit",Just "-1")
+      ,("offset",Just "1")
+      ] 
+      `shouldBe` 
+         Just ("News.creation_date = '2022-03-22' AND name_user = 'Sam' AND \
+                \News.category_id = 11",
+               "ORDER BY CARDINALITY(photo) LIMIT 20 OFFSET 1")      
+  it "test 4" $                                
+    setMethodNews 20 
+      [("created_until",Just "2022-03-22"), ("author",Just "Violette")
+      ,("category",Just "4"), ("search",Just "Hello World!")
+      ,("sort_by",Just "date"), ("limit",Just "0")      
+      ] 
+      `shouldBe` 
+         Just ("News.creation_date < '2022-03-22' AND name_user = 'Violette' \
+                \AND News.category_id = 4 \
+                \AND (content || name_user || name_category) \
+                \ILIKE '%Hello World!%'",
+               "ORDER BY creation_date LIMIT 20 OFFSET 0")            
+  it "test 5" $                                
+    setMethodNews 20 
+      [("created_at",Just "2022-03-22"), ("avthor",Just "Sam")
+      ,("category",Just "11"),("sort_by",Just "photo"), ("limit",Just "10")
+      ,("offset",Just "1")
+      ] `shouldBe` Nothing 
+  it "test 6" $                                
+    setMethodNews 20 
+      [("created_until",Just "2022-03-22"), ("author", Nothing)
+      ,("category",Just "4"), ("title",Just "Test")
+      ,("content",Just "Just"), ("search",Just "Hello World!")
+      ,("sort_by",Just "date"), ("limit",Just "0")      
+      ] 
+      `shouldBe` 
+         Just ("News.creation_date < '2022-03-22' AND name_user = '???' \
+                \AND News.category_id = 4 AND title ILIKE '%Test%' \
+                \AND content ILIKE '%Just%' \
+                \AND (content || name_user || name_category) \
+                \ILIKE '%Hello World!%'",
+               "ORDER BY creation_date LIMIT 20 OFFSET 0")               
+    
+    
+    
+    
 
 
 main :: IO ()
