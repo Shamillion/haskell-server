3fcf1a75a27e5502fb88cb627c12a3b1f749aeb7
Shamil <ns-80@yandex.ru>
2023-10-03 22:48:26 +0500

    Edited scripts and README.md #14

--- _scripts/news/4_getFilteredSortedListNews.sh
+++ _scripts/news/4_getFilteredSortedListNews.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -X GET "http://localhost:8080/news?author=Sam&sort_by=category"
+curl -X GET "http://localhost:8080/news?author=Adam&sort_by=category"
--- _scripts/user/1_addUser.sh
+++ _scripts/user/1_addUser.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-curl -X POST "http://SamLog:BlackPass@localhost:8080/user/create?name_user=Mario&`
+curl -X POST "http://Adam:sixthDay@localhost:8080/user/create?name_user=Mario&`
                    `login=Mario123&pass=Mario123&is_admin=false&is_author=true"
